3919178e22fa07d598364f221c46d9aeb7222152
SebastianZamalloa <104155286+SebastianZamalloa@users.noreply.github.com>
2023-06-25 21:29:59 -0500

    Create 1.cpp

---
+++ Complejidad/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int count = 0;
+
+    int n;
+    cout<<"Ingrese n: ",cin>>n;
+
+    for(int i = 0; i<=n ; i++, count++) //n+2
+    {
+        count++;if(i%2 == 0) //n+1
+        {
+            count++;
+            cout<<i<<endl;  //(n+1)/2
+        }
+    }count++;
+    //2n+3 + n+1/2 = (5n+7)/2
+    cout<<"\nConteo: "<<count<<endl<<endl;
+    //verificado
+}
